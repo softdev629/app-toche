46d064f9308e47665c0cb247372cb18ed76360d4
smartdev1010 <shvab.serhii86@gmail.com>
2023-11-08 13:55:27 +0300

    logout and auth toasts

---
+++ js/auth.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+
+import { firebaseConfig } from "../config.js";
+import { LOGIN_ROUTE, LOGOUT_ROUTE } from "./constant.js";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export const checkAuth = () => {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) location.href = LOGIN_ROUTE;
+  });
+};
+
+export const checkFrom = () => {
+  const from = localStorage.getItem("from");
+  switch (from) {
+    case LOGIN_ROUTE:
+      Toastify({
+        text: "Logged in successfully",
+        style: { background: "#2ecc71" },
+      }).showToast();
+      break;
+    case LOGOUT_ROUTE:
+      Toastify({
+        text: "Logged out successfully",
+        style: { background: "#e74c3c" },
+      }).showToast();
+  }
+};
---
+++ js/constant.js
@@ -0,0 +1,3 @@
+export const LOGIN_ROUTE = "/auth/login.html";
+export const ACCOUNT_ROUTE = "/account.html";
+export const LOGOUT_ROUTE = "/auth/logout";
